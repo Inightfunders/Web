02f38f75272e2eda9b6a5de5ebfc01f590bd2a49
rostislavdev123 <rostislav.dev123@protonmail.com>
2025-03-10 08:28:25 -0300

    fixed build issue

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,16 +8,16 @@ export default defineConfig({
   out: './migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL || "",
+    url: process.env.DATABASE_URL || '',
     // database: "postgres",
     // port: 5432,
     // host: "aws-0-ap-south-1.pooler.supabase.com",
     // user: "postgres.user",
     // password: process.env.PW || "",
-    database: "postgres",
+    database: 'postgres',
     port: 5432,
-    host: "aws-0-us-east-1.pooler.supabase.com",
-    user: "postgres.zaizjasuduohwngmlxil",
-    password: process.env.DATABASE_PW || "",
-  },
+    host: 'aws-0-us-east-1.pooler.supabase.com',
+    user: 'postgres.zaizjasuduohwngmlxil',
+    password: process.env.DATABASE_PW || ''
+  }
 });
